5f45b78f2d8edd5afcd464cafb60d6a7b3bacf32
Benjamin Geyer <brgeyer49@gmail.com>
2017-06-01 16:08:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested div div div div')
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+n
+  }
+}
+
+function deepestChild(){
+  let x = document.querySelector('div#grand-node')
+  let temp = x.children[0]
+  let final = x.children[0]
+
+  while(final.children[0]!=undefined){
+    final = temp.children[0]
+    temp = final.children[0]
+  }
+  return final
+
+}
