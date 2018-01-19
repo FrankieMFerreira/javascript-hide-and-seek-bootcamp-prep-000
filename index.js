8537b94e925b44aa508bd90215686f8a95c4ec77
Zachary Ciatteo <zciatteo96@gmail.com>
2018-01-19 20:44:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function getFirstSelector(selector){
   return document.querySelector(selector);
 }
+
+function nestedTarget(){
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function deepestChild(){
+  return document.getElementById("grand-node").querySelector("div div div div");
+}
+
+function increaseRankBy(n){
+  document.querySelectorAll(".ranked-list li").forEach(function(item){
+    item.innerHTML = parseInt(item.innerHTML) + n;
+  })
+}
